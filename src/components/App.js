47bb3548894b309230092d62cb157892c92e792d
Jim Stahl <jimcool287@gmail.com>
2021-03-24 22:55:02 -0500

    Added 404 page

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import HomePage from './HomePage';
 import TicketList from './TicketList';
 import Header from './common/Header';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import PageNotFound from './PageNotFound'
 
 function App()
 {
     return (
         <div className='container-fluid'>
             <Header/>
-            <Route path= '/' exact component={HomePage}/>
-            <Route path= '/TicketList' exact component={TicketList}/>
+            <Switch>
+                <Route path= '/' exact component={HomePage}/>
+                <Route path= '/TicketList' exact component={TicketList}/>
+                <Route component={PageNotFound}/>
+            </Switch>
         </div>
     );
 }
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PageNotFound() {
+    return (
+        <div className='jumbotron'>
+            <h1>Error 404</h1>
+            <p>Page does not exist or was removed.</p>
+        </div>
+    );
+}
+
+export default PageNotFound;
