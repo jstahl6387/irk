4edecd35182f9289cb8473b54761a1f1507ebb52
Jim Stahl <jimcool287@gmail.com>
2021-03-28 23:01:41 -0500

    Added stuff for a mock API

---
+++ tools/mockData.js
@@ -0,0 +1,88 @@
+const tickets = [
+  {
+    type: 'Enchancement',
+    id: 0,
+    project: 'FooBar',
+    priority: 1,
+    title: 'Break up the HTML into react components',
+    owner: 'Jim',
+    status: 'Completed',
+    updated: '2021-03-15 9:00:00'
+  },
+  {
+    type: 'Enchancement',
+    id: 1,
+    project: 'FooBar',
+    priority: 1,
+    title: 'Make the CSS not suck',
+    owner: 'Jim',
+    status: 'In Progress',
+    updated: '2021-03-15 9:00:00'
+  },
+  {
+    type: 'Enchancement',
+    id: 2,
+    project: 'FooBar',
+    priority: 2,
+    title: 'Add sorting to ticket list',
+    owner: 'Jim',
+    status: 'Pending',
+    updated: '2021-03-15 9:00:00'
+  },
+  {
+    type: 'Enchancement',
+    id: 3,
+    project: 'FooBar',
+    priority: 3,
+    title: 'Make data based ticket list',
+    owner: 'Jim',
+    status: 'Pending',
+    updated: '2021-03-15 9:00:00'
+  },
+  {
+    type: 'Enchancement',
+    id: 4,
+    project: 'FooBar',
+    priority: 2,
+    title: 'Add ticket editor',
+    owner: 'Jim',
+    status: 'Pending',
+    updated: '2021-03-15 9:00:00'
+  },
+  {
+    type: 'Enchancement',
+    id: 5,
+    project: 'FooBar',
+    priority: 2,
+    title: 'Be able to select tickets and type in them',
+    owner: 'Jim',
+    status: 'Pending',
+    updated: '2021-03-15 9:00:00'
+  },
+  {
+    type: 'Enchancement',
+    id: 6,
+    project: 'FooBar',
+    priority: 2,
+    title: 'Add unit testing',
+    owner: 'Jim',
+    status: 'Pending',
+    updated: '2021-03-24 23:39:00'
+  }
+];
+
+const newTicket = {
+  id: null,
+  project: "",
+  priority: null,
+  title: "",
+  owner: "",
+  status: "Pending",
+  updated: Date.now()
+};
+
+// Using CommonJS style export so we can consume via Node (without using Babel-node)
+module.exports = {
+  newTicket,
+  tickets
+};
